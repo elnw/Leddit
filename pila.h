092eee32b0ff56631ba0941d778503a0016955dc
elnw <xxelnwxx@gmail.com>
2020-10-02 17:16:43 -0500

    Update pila.h

--- pila.h
+++ pila.h
@@ -11,28 +11,28 @@ private:
     ListaEnlazada<T> *almacenamiento;
     int cantidad;
 public:
-    Pila(){
+    Pila(){ //=O(n) - 2
         this->cantidad = 0;
         this->almacenamiento = new ListaEnlazada<T>();
     }
-    void Push(T item){
+    void Push(T item){// O(1) - 4
         almacenamiento->Agregar(item);
         cantidad++;
     }
-    void Pop(){
+    void Pop(){ //O(1) - 5
         if(cantidad > 0){
             almacenamiento->EliminarInicial();
              cantidad--;
         }
     }
-    T Peek(){
+    T Peek(){ // O(1) - 2
         if(cantidad > 0){
             return almacenamiento->ObtenerInicial();
         }else{
             return nullptr;
         }
     }
-    int ObtenerCantidad(){
+    int ObtenerCantidad(){ //O(1) - 1
         return cantidad;
     }
 };
